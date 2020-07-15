777d3234be5cbbd969cfcd7ec34d776875882aec
masumshaikh <smshaikh@hotmail.com>
2020-07-15 13:41:43 +0100

    Add another question to CubeLinkedList.h

--- CubeImplementations/CubeLinkedList.h
+++ CubeImplementations/CubeLinkedList.h
@@ -15,14 +15,14 @@ private:
 		CubeListNode(Cube c) : next_{ nullptr }, prev_{ nullptr }
 		{
 			data_ = c;  // Invokes copy assignment operator; if we had put data_{ c } in the initialiser list,
-						// that would have invoked copy constructor instead. Neither is desirable.
+                        // that would have invoked copy constructor instead. Neither is desirable.
 
-						// QUESTION 1:  Assuming I don't mind using a copy assignment operator as is currently the case,
-						//              how do I know if the default copy assignment operator is sufficient here?
+                        // QUESTION 1:  Assuming I don't mind using a copy assignment operator as is currently the case,
+                        //              how do I know if the default copy assignment operator is sufficient here?
 			
-						// QUESTION 2:  What's the best way to avoid these copy/copy assignment calls? Should I ...
-						//              - Change data_ to be a Cube*
-						//				- Change the signature of this constructor to be CubeListNode(const Cube& c)
+                        // QUESTION 2:  What's the best way to avoid these copy/copy assignment calls? Should I ...
+                        //              - Change data_ to be a Cube*
+                        //				- Change the signature of this constructor to be CubeListNode(const Cube& c)
 		}
 	};
 
@@ -37,12 +37,14 @@ public:
 	~CubeLinkedList();
 
 	Cube& operator[](int index);    // QUESTION 4: The STL library always has const versions of these overloads ...
-									// ... Why are they necessary?
+                                    // ... Why are they necessary?
 	
 	//int PeekFront();
 	//int PeekBack();
 
-	void InsertAtFront(const Cube& data);
+	void InsertAtFront(const Cube& data);	// QUESTION 5. My implementation involves a copy constructor. It starts with:
+                                            // CubeListNode* newHead = new CubeListNode(data);
+	                                        // How to avoid this?
 	//void InsertAtEnd();
 	//void InsertAtIndex();
 	//void PopFromFront();
